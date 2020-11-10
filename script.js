619402dc8ae2c9c0f3a3f16f1f84fde656e0abc8
Victor <viktor-lapshov@bk.ru>
2020-11-10 21:26:18 +0300

    Это наш первыый комит

---
+++ script.js
@@ -0,0 +1,30 @@
+/*jQuery('document').ready(function(){
+
+
+}); */
+
+//console.log(typeof(null));
+'use strict';
+a = 5;
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isClose);
+
+
+
+
+
+
