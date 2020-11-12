92afd94f41e15634678348f3e06a04cca95b0979
Victor <viktor-lapshov@bk.ru>
2020-11-12 14:55:16 +0300

    Задание 3

---
+++ chooseOptExpenses.js
@@ -0,0 +1,6 @@
+function chooseOptExpenses() {
+  let q1 = prompt("Статья необязательных расходов?");
+  while (q1++, q1 = 3);
+
+}
+chooseOptExpenses();
--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-let money,
-    time;
+let money,time;
+function start() {
+  money= +prompt("Ваш бюджет на месяц?", "");
+  time = prompt('ВВедите дату в формате YYYY-MM-DD', '');
 
-money = +prompt("Ваш бюджет на месяц?");
-time = prompt ("Введите дату в формате YYYY-MM-DD");
+  while(isNaN(money)|| money =="" || money == null ) {
+    money= +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+start();
+  
 
- let income;
- let appData = {
-  budget:money,
-  timeData:time,
+let appData = {
+  budget: money,
   expenses: {},
-  optionalExpenses: {},
   income: [],
-  savings:false 
-
-  };
-
-  let a,b;
+  timeData: time,
+  savings: true
+};
 
 
+
+let a,b;
+
+function chooseExpenses() {
 for(let i = 0; i < 2; i++) {
   a = prompt("Введите обязательную статью расходов в этом месяце", ""),
   b = prompt("Во сколько обойдется?","");
@@ -27,15 +32,22 @@ for(let i = 0; i < 2; i++) {
     appData.expenses[a] = b;
 
   } else{
-
+     i = i -1;
   }
-  
-};
+
+}
+}
+chooseExpenses();
+
+function detectDayBudget() {
 let oneday;
-appData.moneyPerDay=oneday/30;
-
+appData.moneyPerDay=(oneday/30).toFixed();
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
 
+
+function detectLevel() {
 if(appData.moneyPerDay <100) {
   console.log ("Минимальный уровень достактка")
 } else if (appData.moneyPerDay >100 && appData.moneyPerDay < 2000) {
@@ -45,7 +57,29 @@ if(appData.moneyPerDay <100) {
 } else{
   console.log("Произошла ошибка");
 }
+}
+detectLevel();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt("Какова сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
+  appData.mothIncome = save/100/12*percent;
+  alert("Доход в месяц с вашего депозита: " + appData.mothIncome);
+  }
+}
+checkSavings();
 
 
-
-   
+function chooseOptExpenses() {
+  let q1 = prompt("Статья необязательных расходов?"),
+      q2 = prompt("Статья необязательных расходов?"),
+      q3 = prompt("Статья необязательных расходов?");
+      optionalExpenses: {
+        console.log('1',q1),
+        console.log('2',q2),
+        console.log('3',q3);
+      }
+  
+}
+chooseOptExpenses();
